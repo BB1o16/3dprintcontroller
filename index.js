fae9874486518d7bb491479906eec05e1851ce83
Brandon <brandon10161996@gmail.com>
2020-10-29 15:18:27 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+// The HelloWorld
+app.get("/", (req, res) => {
+    res.send("Hello from Node.js!");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`App listening on http://localhost:${port}`);
+});
