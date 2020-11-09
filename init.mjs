72ad14cd6dc8b9b789444784b6d5cc32db652e4c
Brandon <brandon10161996@gmail.com>
2020-11-09 13:53:00 -0500

    changed com port

--- init.mjs
+++ init.mjs
@@ -1,6 +1,6 @@
 import {Printer} from "3d-printer-controller";
 
 (async () => {
-    const myPrinter = new Printer("COM6", 115200, {x: 220, y: 220, z: 300});
+    const myPrinter = new Printer("COM3", 115200, {x: 220, y: 220, z: 300});
     await myPrinter.init();
 })();
